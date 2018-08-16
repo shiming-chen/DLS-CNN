65f424be5a4ca2d3013821cc508c74f99735a2a1
Shiming Chen <g_shmchen@163.com>
2018-08-16 10:59:46 +0800

    Update test.m

--- ResNet/test/test.m
+++ ResNet/test/test.m
@@ -13,6 +13,38 @@ net.move('gpu') ;
 net.conserveMemory = true;
 im_mean = net.meta(1).normalization.averageImage;
 %im_mean = imresize(im_mean,[224,224]);
+
+% feature extraction for test set
+p = dir('/home/lab535-user1/chenshm/Writer_Ident/data/ICDAR2013/icdar2013_test_Line/icdar2013_train_all_Line_patches256_new/*jpg');
+ff = [];
+%------------------------------
+for i = 1:200:numel(p)
+    disp(i);
+    oim = [];
+    str=[];
+    for j=1:min(200,numel(p)-i+1)
+        str = strcat('/home/lab535-user1/chenshm/Writer_Ident/data/ICDAR2013/icdar2013_test_Line/icdar2013_train_all_Line_patches256_new/',p(i+j-1).name);
+        %imt = imresize(imread(str),[256,256]);
+	imt=imread(str);
+        oim = cat(4,oim,imt);
+    end
+    f = getFeature2(net,oim,im_mean,'data','fc1n');
+    f = sum(sum(f,1),2);
+    f2 = getFeature2(net,fliplr(oim),im_mean,'data','fc1n');
+    f2 = sum(sum(f2,1),2);
+    f = f+f2;
+    size4 = size(f,4);
+    f = reshape(f,[],size4)';
+    s = sqrt(sum(f.^2,2));
+    dim = size(f,2);
+    s = repmat(s,1,dim);
+    f = f./s;
+    ff = cat(1,ff,f);
+end
+save('../test/icdar2013_all_fc_patches256_tain.mat','ff','-v7.3');
+%}
+
+% feature extraction for test set
 p = dir('/home/lab535-user1/chenshm/Writer_Ident/data/ICDAR2013/icdar2013_test_Line/icdar2013_test_all_Line_patches256_new/*jpg');
 ff = [];
 %------------------------------
@@ -39,8 +71,7 @@ for i = 1:200:numel(p)
     f = f./s;
     ff = cat(1,ff,f);
 end
-save('../test/icdar2013_all_pool5_patches256.mat','ff','-v7.3');
-%}
+save('../test/icdar2013_all_fc_patches256_test.mat','ff','-v7.3');
 
 
 
